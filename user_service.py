044656be8034a48bcec98d241c0edddad9f2dad5
Oskar Pietrzak <u114_oskpie_waw@technischools.com>
2024-03-01 10:07:06 +0100

    crateuser do zrobienia

--- user_service.py
+++ user_service.py
@@ -6,5 +6,18 @@ class UserService:
     def get_all_users(self):
         return list(self.users.values())
 
-    def get_user_by_id(self, user_id):
+    def get_user(self, user_id):
         return self.users.get(user_id)
+
+    def update_user(self, user_id, data):
+        user = self.users.get(user_id)
+        if not user:
+            return "User not found", 404
+        user.update(data)
+        return user, 200
+
+    def delete_user(self, user_id):
+        if user_id not in self.users:
+            return "User not found", 404
+        del self.users[user_id]
+        return "User deleted successfully", 200
