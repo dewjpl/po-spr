5948be7910ef496e7169e325b8f7fe6ec39f814e
Oskar Pietrzak <u114_oskpie_waw@technischools.com>
2024-03-01 09:57:43 +0100

    post i get

--- main1.py
+++ main1.py
@@ -9,5 +9,11 @@ def get_users():
     users = user_service.get_all_users()
     return jsonify(users), 200
 
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    result, code = user_service.create_user(data)
+    return jsonify(result), code
+
 if __name__ == '__main__':
     app.run(debug=True)
